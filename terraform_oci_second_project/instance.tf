4422be630ead1293e638f4ac1a7bc388e272d78e
JJ Warzecha <jj.warzecha@gmail.com>
2019-11-06 16:33:45 +0100

    #jj - add bm instance

--- terraform_oci_second_project/instance.tf
+++ terraform_oci_second_project/instance.tf
@@ -27,3 +27,31 @@ resource "oci_core_instance" "test_instance" {
     create = "60m"
   }
 }
+
+resource "oci_core_instance" "bm_instance" {
+  availability_domain = "${data.oci_identity_availability_domain.ad.name}"
+  compartment_id      = "${var.compartment_ocid}"
+  display_name        = "${var.prefix}MBInstance"
+  shape               = "BM.Standard1.36"
+
+  create_vnic_details {
+    subnet_id        = "${oci_core_subnet.test_subnet.id}"
+    display_name     = "Primaryvnic"
+    assign_public_ip = true
+    hostname_label   = "tfexampleinstance${count.index}"
+  }
+
+  source_details {
+    source_type = "image"
+    source_id   = "${var.instance_image_ocid[var.region]}"
+  }
+
+  metadata = {
+    ssh_authorized_keys = "${file("./.ssh/id_rsa.pub")}"
+    user_data           = "${base64encode(file("./userdata/bootstrap"))}"
+  }
+
+  timeouts {
+    create = "60m"
+  }
+}
